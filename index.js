3cb71cabc1cbd76c894a27c0d433c94cf75f37d6
loren <lorenmichaelh@gmail.com>
2022-01-24 16:54:16 -0600

    commit tree c67f3069c4aa21280753942aeb6eb0766a6521fb

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize(str) {
+    return str[0].toUpperCase() + str.substring(1)
+  }
+
+  static sanitize(str) {
+    return str.replace(/[&\/\\#^+()$~%.":*?<>{}!@]/g, '')
+  }
+
+  static titleize(str) {
+    const excludedWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    const splitStr = str.split(' ');
+    let newStr = [];
+    for (let i = 0; i < splitStr.length; i++)
+    {
+      if (i === 0 || !excludedWords.includes(splitStr[i]))
+      {
+        newStr.push(this.capitalize(splitStr[i]));
+      }
+      else
+      {
+        newStr.push(splitStr[i]);
+      }
+    }
+    return newStr.join(' ');
+  }
 }
